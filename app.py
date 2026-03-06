edd3ce8fac0ab3bc880a6fe8162c8cf2bcd84b7b
Sithumi Samadhi <147872090+SamadhiDBS@users.noreply.github.com>
2026-03-06 20:05:47 +0530

    Update main execution block to specify host and port
    Change app.run to use host and port parameters.

--- app.py
+++ app.py
@@ -65,5 +65,5 @@ def index():
     return render_template('index.html', pred_value=pred_value)
 
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=7860) 
